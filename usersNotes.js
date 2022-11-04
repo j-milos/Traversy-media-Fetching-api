932d289bc536634756e3fac381537d56775e47de
Milos Jovanovic <jovanovic.milos977@gmail.com>
2022-11-04 23:45:32 +0100

    nodejssample-one

---
+++ usersNotes.js
@@ -0,0 +1,20 @@
+const users = 
+// Array of objects
+[
+    {
+        "id": 1,
+        "name": "Rick",
+        "email": "rick@gmail.com"
+    },
+    // Object
+    {
+        "id": 2,
+        "name": "John",
+        "email": "john@gmail.com"
+    },
+    {
+        "id": 1,
+        "name": "Megan",
+        "email": "megan@gmail.com"
+    }
+]
